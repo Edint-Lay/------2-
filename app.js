faa51150c1aa1f1c06dac25161703e8e6e2e1fc8
Edint-Lay <wngur89@edint.io>
2022-11-21 19:35:13 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const compress = require("compression");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const maria = require("mariadb");
+const app = express();
+
+require("dotenv").config();
+
+app.use(cors({
+    origin: "*",
+    methods: "GET, HEAD, PUT, PATCH, POST, DELETE"
+  }));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(compress());
+app.use(cookieParser());
+
+app.post("/rooms", async(req, res) => {
+    const 
+})
+
+app.listen(port, () =>
+  console.log('Example app listening on port 3001!'),
+);
